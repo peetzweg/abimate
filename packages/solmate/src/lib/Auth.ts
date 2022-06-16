6074f1eff395fdc88f334c449d3767591e542113
pczek <839848+peetzweg@users.noreply.github.com>
2022-06-16 17:47:22 +0200

    adds library code and ignores submodule

---
+++ packages/solmate/src/lib/Auth.ts
@@ -0,0 +1,2 @@
+export { Auth } from '../../types/ethers-contracts';
+export { abi as AuthABI } from '../../lib/out/Auth.sol/Auth.json';
---
+++ packages/solmate/src/lib/ERC721.ts
@@ -0,0 +1,2 @@
+export { ERC721 } from '../../types/ethers-contracts';
+export { abi as ERC721ABI } from '../../lib/out/ERC721.sol/ERC721.json';
